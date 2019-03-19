5484245d3814f00551ed279c7aa6acd81858346b
saman <abdallaelsaman67@gmail.com>
2019-03-19 16:26:52 +0200

    find loops bugs free yarab b2a

--- src/Loop.java
+++ src/Loop.java
@@ -2,35 +2,45 @@ import java.util.ArrayList;
 
 public class Loop {
 	ArrayList<Integer> nodes = new ArrayList<>();
-	int hashValue = 1;
+	//int hashValue = 1;
 
 	public Loop(ArrayList<Integer> nodes) {
 		this.nodes = nodes;
 
-		if (nodes.size() == 2) {
-			for (int s: nodes) {
-				hashValue *= s + 1;
-			}
-			return;
-		}
-
-		int i = 0;
-
-		for (int s: nodes) {
-			i++;
-			if (i%2 == 0) {
-				hashValue *= s + 1;
-			} else {
-				hashValue *= s + 2;
-			}
-		}
+//		if (nodes.size() == 2) {
+//			for (int s: nodes) {
+//				hashValue *= s + 1;
+//			}
+//			return;
+//		}
+//
+//		int i = 0;
+//
+//		for (int s: nodes) {
+//			i++;
+//			if (i%2 == 0) {
+//				hashValue *= s + 1;
+//			} else {
+//				hashValue *= s + 2;
+//			}
+//		}
 	}
 
 	boolean isEqualTo (Loop loop) {
-		if (loop.nodes.size() == this.nodes.size() && loop.hashValue == this.hashValue) {
+		if (loop.nodes.size() == this.nodes.size()) {
+			int index2 = loop.nodes.indexOf(this.nodes.get(0));
+			if (index2 == -1) {
+				return false;
+			}
+
+			for (int i = 1; i < this.nodes.size(); i++) {
+				index2 = (i + 1) % (loop.nodes.size());
+				if (this.nodes.get(index2) != loop.nodes.get(i)) {
+					return false;
+				}
+			}
 			return true;
-		} else {
-			return false;
 		}
+		return false;
 	}
 }
