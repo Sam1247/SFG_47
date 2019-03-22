6ee83fb77955f5c3c2c27d99d6b5f732b11c61e8
saman <abdallaelsaman67@gmail.com>
2019-03-22 16:09:50 +0200

    Refactoring all classes (delete comments)

--- src/Loop.java
+++ src/Loop.java
@@ -2,27 +2,9 @@ import java.util.ArrayList;
 
 public class Loop {
 	ArrayList<Integer> nodes = new ArrayList<>();
-	//int hashValue = 1;
 	public Loop(ArrayList<Integer> nodes) {
 		this.nodes = nodes;
 
-//		if (nodes.size() == 2) {
-//			for (int s: nodes) {
-//				hashValue *= s + 1;
-//			}
-//			return;
-//		}
-//
-//		int i = 0;
-//
-//		for (int s: nodes) {
-//			i++;
-//			if (i%2 == 0) {
-//				hashValue *= s + 1;
-//			} else {
-//				hashValue *= s + 2;
-//			}
-//		}
 	}
 
 	int getGainFrom (ArrayList<Node>[] graphNodes) {
--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,6 @@ public class Main {
 
 		int size = scanner.nextInt();
 		int edges = scanner.nextInt();
-		//ArrayList<Node> Graph = new ArrayList<Node>();
-
-		//ArrayList<Node> [] graph = new ArrayList[size];
 
 		Graph graph = new Graph(size);
 
@@ -25,45 +22,6 @@ public class Main {
 			graph.nodes[from].add(new Node(to, gain));
 		}
 
-		//graph.printLoops();
-//		ArrayList  arrl = new ArrayList();
-//		arrl.add(1);
-//		arrl.add(3);
-//		arrl.add(2);
-//
-//		ArrayList  arrl2 = new ArrayList();
-//		arrl2.add(3);
-//		arrl2.add(2);
-//		arrl2.add(1);
-//
-//
-//
-//		Loop test = new Loop(arrl);
-//		Loop test2 = new Loop(arrl2);
-//
-//		System.out.println(test.isEqualTo(test2));
-
-		//ArrayList<ArrayList<Integer>> test = new ArrayList<ArrayList<Integer>>();
-
-
-		//test = graph.getforwardPathes();
-		//graph.findLoops(0);
-		//graph.printLoops();
-		//graph.printForwardPaths();
-//		for (int i = 0; i < test.size(); ++i) {
-//			for (int j= 0; j < test.get(i).size(); j++) {
-//				System.out.print(test.get(i).get(j));
-//				System.out.print(" ");
-//			}
-//			System.out.println();
-//		}
-//
-
-
-		//graph.printDelta();
-		//System.out.println(graph.getBigDelta());
-		//System.out.println(graph.getNunumerator());
-//		graph.printLoops();
 		System.out.println("Forward paths:");
 		graph.printForwardPaths();
 		System.out.println("All Loops:");
